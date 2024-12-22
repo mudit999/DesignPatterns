d86a57b2dadba457b7174f8e28197044a573b9d8
mudit999 <muditgarg1211@gmail.com>
2024-12-22 16:04:13 +0530

    File renamed

--- src/main/java/SingletonPattern/DBConnectionDoubleCheck.java
+++ src/main/java/SingletonPattern/DBConnectionDoubleCheck.java
@@ -1,20 +1,21 @@
 package SingletonPattern;
 
 // Database Connection class with Double Check Locking
-public class DatabaseConnectionDoubleLocking {
-    private static DatabaseConnectionDoubleLocking instance = null;
+//
+public class DBConnectionDoubleCheck {
+    private static DBConnectionDoubleCheck instance = null;
 
     String url;
     String username;
     String password;
 
-    private DatabaseConnectionDoubleLocking(){}
+    private DBConnectionDoubleCheck(){}
 
-    public static DatabaseConnectionDoubleLocking getInstance(){
+    public static DBConnectionDoubleCheck getInstance(){
         if(instance == null){
             synchronized (instance) {
                 if(instance == null){
-                    instance = new DatabaseConnectionDoubleLocking();
+                    instance = new DBConnectionDoubleCheck();
                 }
             }
         }
